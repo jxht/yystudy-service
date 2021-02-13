dbec0197e478f7d9a43e6aa78651cfe382504044
zhangyanqiang <zhangyanqiang@meicai.cn>
2021-02-13 20:53:15 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('user', 'UserController');
+
 Route::any('radical/type-list','RadicalController@typeList');
