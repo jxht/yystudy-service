3a33ef0a05932e10db4409573db60c329f54e9ad
zhangyanqiang <zhangyanqiang@meicai.cn>
2021-02-13 15:59:05 +0800

    first commit

---
+++ app/Models/Dictionary/Radical.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Dictionary;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Radical extends Model
+{
+    use HasFactory;
+}
